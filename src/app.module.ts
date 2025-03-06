970adb3363662a9705ab9622ffb6b034d8f855e7
ibrahimabdalrhman <ibrahimabdalr7man@gmail.com>
2025-03-06 17:43:37 +0200

    chore: load environment variables from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { FoldersModule } from './folders/folders.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres', 
-      password: 'pass123', 
-      database: 'notes-api', 
+      host: process.env.DB_HOST ,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME, 
       autoLoadEntities: true,
       synchronize: true, 
     }),    
